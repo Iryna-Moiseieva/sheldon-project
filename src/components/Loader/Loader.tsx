9f1567bbb5dbabf8382fc2c853318dd8cac23ba4
Iryna Moiseieva <arristotell8@gmail.com>
2023-04-16 12:36:48 +0200

    added loader layout

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import './Loader.css';
 
-function Loader() {
-  return (
-    <>
-    </>
-  );
+import { Container, Ellipse } from './Loader.styles.js';
+
+interface LoaderProps {
+  absolute: boolean;
 }
+
+const Loader: React.FC<LoaderProps> = ({ absolute = false }) => {
+  return (
+    <Container absolute={absolute}>
+      <Ellipse></Ellipse>
+      <Ellipse></Ellipse>
+      <Ellipse></Ellipse>
+      <Ellipse></Ellipse>
+    </Container>
+  );
+};
 
 export default Loader;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+
 import './App.css';
+import Loader from '../../components/Loader';
 
-function App() {
-
-  return <h1 className="app">Sheldon Project</h1>;
+interface AppProps {
+  absolute: boolean;
 }
+const App: React.FC<AppProps> = ({ absolute = false }) => {
+
+  return (
+    <>
+      <h1 className="app">Sheldon Project</h1>;
+      <Loader absolute={absolute}/>
+    </>
+  );
+};
 
 export default App;
